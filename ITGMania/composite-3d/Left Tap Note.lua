c11d411ab4443eb868bc22289e5221bcf13b7613
PoRa-dayo <62925753+PoRa-dayo@users.noreply.github.com>
2025-11-22 16:02:52 +0700

    composite for outfox and itgmania

---
+++ ITGMania/composite-3d/Left Tap Note.lua
@@ -0,0 +1,7 @@
+local t = Def.Model {
+	Meshes=NOTESKIN:GetPath('_down','tap note model');
+	Materials=NOTESKIN:GetPath('_left','tap note materials');
+	Bones=NOTESKIN:GetPath('_down','tap note model');
+};
+
+return t;
