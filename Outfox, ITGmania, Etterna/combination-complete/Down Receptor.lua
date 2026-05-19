0ddac0bfcef36f5192d5da4955dc793acb67e163
PoRa-dayo <62925753+PoRa-dayo@users.noreply.github.com>
2026-05-20 01:15:49 +0700

    bruh

--- Outfox, ITGmania, Etterna/combination-complete/Down Receptor.lua
+++ Outfox, ITGmania, Etterna/combination-complete/Down Receptor.lua
@@ -1,4 +1,4 @@
---If a Command has "NOTESKIN:GetMetricA" in it, that means it gets the command from the metrics.ini, else use cmd(); to define command.
+--If a Command has "NOTESKIN:GetMetricA" in it, that means it gets the command from the metrics.ini, else use a function to define command.
 --If you dont know how "NOTESKIN:GetMetricA" works here is an explanation.
 --NOTESKIN:GetMetricA("The [Group] in the metrics.ini", "The actual Command to fallback on in the metrics.ini");
 
@@ -13,7 +13,9 @@ local t = Def.ActorFrame {
 		Delay0002=1;
 		Frame0003=3;
 		Delay0003=1;
-		InitCommand=cmd(effectclock,"beat";diffuseramp;effectcolor1,0.1,0.1,0.1,1;effectcolor2,1,1,1,1;effectperiod,0.5;effecttiming,0.25,0.50,0,0.25;effectoffset,-0.25);
+		InitCommand=function(self)
+			self:effectclock("beat"):diffuseramp():effectcolor1(0.1,0.1,0.1,1):effectcolor2(1,1,1,1):effectperiod(0.5):effecttiming(0.25,0.50,0,0.25):effectoffset(-0.25)
+		end;
 		NoneCommand=NOTESKIN:GetMetricA("ReceptorArrow", "NoneCommand");
 		PressCommand=NOTESKIN:GetMetricA("ReceptorArrow", "PressCommand");
 		LiftCommand=NOTESKIN:GetMetricA("ReceptorArrow", "LiftCommand");
@@ -35,10 +37,16 @@ local t = Def.ActorFrame {
 		Delay0003=1;
 		Frame0004=0;
 		Delay0004=0,75;
-		InitCommand=cmd(blend,'BlendMode_Add';diffusealpha,0);
+		InitCommand=function(self)
+			self:blend('BlendMode_Add'):diffusealpha(0)
+		end;
 		NoneCommand=NOTESKIN:GetMetricA("ReceptorArrow", "NoneCommand");
-		PressCommand=cmd(diffusealpha,0.2);
-		LiftCommand=cmd(diffusealpha,0);
+		PressCommand=function(self)
+			self:diffusealpha(0.2)
+		end;
+		LiftCommand=function(self)
+			self:diffusealpha(0)
+		end;
 		W5Command=NOTESKIN:GetMetricA("ReceptorArrow", "W5Command");
 		W4Command=NOTESKIN:GetMetricA("ReceptorArrow", "W4Command");
 		W3Command=NOTESKIN:GetMetricA("ReceptorArrow", "W3Command");
