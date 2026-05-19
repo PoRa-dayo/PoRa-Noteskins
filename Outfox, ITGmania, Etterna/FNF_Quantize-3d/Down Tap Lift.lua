d54707dde6703974ffc6acfdab6f46157a5e8ac5
PoRa-dayo <62925753+PoRa-dayo@users.noreply.github.com>
2026-05-19 13:35:52 +0700

    more fnf

---
+++ Outfox, ITGmania, Etterna/FNF_Quantize-3d/Down Tap Lift.lua
@@ -0,0 +1,7 @@
+local t = Def.Model {
+	Meshes=NOTESKIN:GetPath('_down','tap lift meshes');
+	Materials=NOTESKIN:GetPath('_down','tap lift meshes');
+	Bones=NOTESKIN:GetPath('_down','tap lift meshes');
+};
+
+return t;
---
+++ Outfox, ITGmania, Etterna/FNF_Quantize-3d/Down Tap Mine.lua
@@ -0,0 +1,8 @@
+local t = Def.Model {
+	Meshes="_mine meshes down.txt";
+	Materials="_mine meshes down.txt";
+	Bones="_mine meshes down.txt";
+};
+
+return t;
+
